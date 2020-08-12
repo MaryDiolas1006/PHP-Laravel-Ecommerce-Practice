6e9251627f7cf38956d74f6359a7f830b384e48c
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-08-12 16:37:27 +0800

    add relationships to Models

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    public function status()
+    {
+    	return $this->belongsTo('App\Status');
+
+    }
+
+    public function paymentMode()
+    {
+    	return $this->belongsTo('App\paymentMode');
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+
+    public function products()
+    {
+    	return $this->belongsToMany('App\Product')
+    		->withPivot('subtotal', 'price', 'quantity')
+    		->withTimestamps();
+    }
 }
