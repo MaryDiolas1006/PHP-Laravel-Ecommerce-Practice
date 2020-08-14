fa420d9cd7a208a9226b57658c23783bc0c0e2e3
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-08-14 08:56:45 +0800

    added ternary operator to badge

--- resources/views/transactions/partials/accordion.blade.php
+++ resources/views/transactions/partials/accordion.blade.php
@@ -9,7 +9,7 @@
       <h2 class="mb-0">
         <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse{{$transaction->id}}" aria-expanded="true" aria-controls="collapseOne">
           {{$transaction->transaction_code}}
-          <span class="badge badge-{{-- {{$transaction->status_id === 1 ? "success" && $transaction->status_id === 2 ? "info" : "danger"}} --}}">
+          <span class="badge badge-{{$transaction->status_id === 1 ? "success" : ($transaction->status_id === 2 ? "info" : "danger")}}">
             {{$transaction->status->name}}
           </span>
         </button>
